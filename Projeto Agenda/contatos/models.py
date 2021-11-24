e8612845a27277b9c4981a795083eea261199a07
Lucas Araújo <lucasaraujoti73@gmail.com>
2021-11-24 18:30:29 -0300

    Projeto agenda com models criados

---
+++ Projeto Agenda/contatos/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+
+"""
+CONTATOS
+id: INT (automático)
+nome: STR * (obrigatório)
+sobrenome: STR (opcional)
+telefone: STR * (obrigatório)
+email: STR (opcional)
+data_criacao: DATETIME (automático)
+descricao: texto
+categoria: CATEGORIA (outro model)
+
+CATEGORIA
+id: INT
+nome: STR * (obrigatório)
+
+"""
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+    
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
+
+
