9e29c0751c834f0f2b77badb76adec5bd44e35ec
Lucas Araújo <lucasaraujoti73@gmail.com>
2021-12-27 13:16:17 -0300

    atualizada django-admin

--- Projeto Agenda/contatos/admin.py
+++ Projeto Agenda/contatos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Categoria, Contato
 # Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'sobrenome','telefone','email','categoria')
+    list_display_links= ('id','nome','sobrenome')
+    list_filter = ('nome','sobrenome')
+    list_per_page = 10
+    search_fields = ('nome','sobrenome','telefone')
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- Projeto Agenda/contatos/models.py
+++ Projeto Agenda/contatos/models.py
@@ -19,6 +19,9 @@ nome: STR * (obrigatório)
 """
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
     
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -28,5 +31,8 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria,on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
 
 
